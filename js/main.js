ebd2e9e05d285df5ed2eadb662f64b2451ba2c49
NutoFinche <butif800@163.com>
2020-07-15 23:51:55 +0800

    Site updated: 2020-07-15 23:51:42

--- js/main.js
+++ js/main.js
@@ -141,12 +141,12 @@ require([], function (){
     }
 
     // Random Color 边栏顶部随机颜色
-    var colorList = ["#6da336", "#ff945c", "#66CC66", "#99CC99", "#CC6666", "#76becc", "#c99979", "#918597", "#4d4d4d"];
+/*     var colorList = ["#6da336", "#ff945c", "#66CC66", "#99CC99", "#CC6666", "#76becc", "#c99979", "#918597", "#4d4d4d"];
     var id = Math.ceil(Math.random()*(colorList.length-1));
     // PC
     $("#container .left-col .overlay").css({"background-color": colorList[id],"opacity": .3});
     // Mobile
-    $("#container #mobile-nav .overlay").css({"background-color": colorList[id],"opacity": .7});
+    $("#container #mobile-nav .overlay").css({"background-color": colorList[id],"opacity": .7}); */
 
     // Table
     $("table").wrap("<div class='table-area'></div>");
@@ -156,7 +156,7 @@ require([], function (){
         if ($("#comments").length < 1) {
             $("#scroll > a:nth-child(2)").hide();
         }
-    })
+    }) 
 
     // Hide Labels
     if(yiliaConfig.isArchive || yiliaConfig.isTag || yiliaConfig.isCategory) {
